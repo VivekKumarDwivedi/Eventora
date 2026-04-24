4ea07c436e4a65e07e5c0e421486747bdda7c3ad
Vivek Kumar Dwivedi <vivekdwivedi498@gmail.com>
2026-04-24 15:58:32 +0530

    Added Mongodb connection setup

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { connectDB } from './src/config/db.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
+
+connectDB();
 
 const PORT = process.env.PORT ||5000;
 
